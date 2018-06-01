472a0af60c211119a146b031b7666f1df42a6daf
Meet Pandya <pandyama2@gmail.com>
2018-06-01 14:27:09 -0400

    changes applied and executable created

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const http = require ('https'); //Constant that requires the HTTPS package in order to make requests to the API
+const http= require ('https'); //Constant that requires the HTTPS package in order to make requests to the API
+// var keypress = require('keypress');
+// //keypress(process.stdin);
+// process.stdin.on('keypress',function(ch, key){
+// 	//console.log('got keypress',key);
+// 	if(key && key.ctrl && key.name == 'c'){
+// 		process.exitCode;
+// 	}
+// });
 
 var url; //Holds the API URL
 
@@ -23,19 +31,22 @@ rl.question('Please enter the date to do currency conversion for: Format (yyyy-d
 	x = date.split('-'); //Split function used to split the Date value by DASH
 
 	//All the user input is nested inside one another
-	rl.question('Please enter the base currency for conversion: ',(ans)=>{
+	rl.question('Base Currency (GBP,CAD,USD....): ',(ans)=>{
 		baseCurrency = String(ans);// baseCurrency holds the value user puts, such as GBP, USD...
 
 		//Another Nested user input prompt
-		rl.question('Please enter the amount to convert: ',(ans)=>{
+		rl.question('Conversion Amount(number only): ',(ans)=>{
 			baseAmount = String(ans); //baseAmount holds the value for the amount to be converted....
 
 			//One more neste user input statement
-			rl.question('Please enter the currency to convert the amount to: ',(ans)=>{
+			rl.question('Conversion Currency(GBP,CAD,USD....): ',(ans)=>{
 				convertCurrency = String(ans); //convertCurrency holds the value user gives, such as the final currency to convert to
 
 				//Below is the modified URL for the API Call where Date, Base Currency and Converted Currency variables are inserted
 				url = `https://exchangeratesapi.io/api/${date}?symbols=${baseCurrency},${convertCurrency}`;
+				//console.log(convertCurrency);
+
+
 				for(var i = 0; i < 3; i++){
 
 						if (x[i] > 999){
@@ -48,47 +59,49 @@ rl.question('Please enter the date to do currency conversion for: Format (yyyy-d
 							arr[2] = x[i];
 						}
 					}
-				getRate();// Calls the getRate() function, which returns 
-				
-					
+				getRate();// Calls the getRate() function, which returns
+ 
+
 				rl.close();
+				process.stdin.resume();
 			});
 		});
 	});
 
 });
 
+
+
 function getRate(){
 
 	var jsonObj; //The Main JSON Object retrieved when the call to API is made
-	var ratesObj; // Captures Rates from the jsonObject 
+	var ratesObj; // Captures Rates from the jsonObject
 	var final; // Holds final converted currency Value
 
 	var finalJson; //JSON object that is displayed as final result
 
 	//Below is the callback function which sends HTTP request to the Exchange Rates API
 	callback = function(response){
-			
+
 			response.on('data',function(chunk){
 				str+=chunk; //collect all the data coming from the API
 				jsonObj = JSON.parse(str); //Parse it into a JSON Object
 			});
 			response.on('end',function(){
 				//console.log(str);
-				console.log('\n On '+jsonObj["date"]+' the rate was: ');
-				console.log(jsonObj["rates"]+'\n');
+				//console.log('\n On '+jsonObj["date"]+' the rate was: ');
+				//console.log(jsonObj["rates"]+'\n');
 
 				ratesObj = jsonObj["rates"];
 				//console.log(ratesObj[`${baseCurrency}`]);
 
-				var final = (baseAmount*ratesObj[`${convertCurrency}`])/ratesObj[`${baseCurrency}`];
+				var final = (baseAmount*ratesObj[convertCurrency])/ratesObj[baseCurrency];
 				//console.log(`Your ${baseAmount} ${baseCurrency} is worth ${final} in ${convertCurrency} with the rate conversion shown above`);
-				
+
 				finalJson = {"Date":`${jsonObj["date"]}`,"Base Currency":`${baseCurrency}`,"Base Amount":`${baseAmount}`,"Conversion Currency":`${convertCurrency}`,"Converted Amount":`${final}`};
-				console.log(finalJson);
+				console.log('Your '+baseAmount+' '+baseCurrency+ ' was worth '+Math.ceil(final,2)+' in '+convertCurrency);
 			});
 		}
 	http.request(url, callback).end();
-	
 
 }
